78398c07cb6658ca904060c80c3ed806d98a21a0
wengliemiao <1057654928@qq.com>
2018-01-08 00:01:56 +0800

    添加注释

--- src/main/java/com/wlm/test/web/RequestMappingTestController.java
+++ src/main/java/com/wlm/test/web/RequestMappingTestController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @RequestMapping 测试控制器
+ * {@link RequestMapping} 测试控制器
  *
  * @author wengliemiao
  */
